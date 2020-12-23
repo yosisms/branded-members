0c8e52be9b32256d26c7afb1ddb0f4f4f82cbeda
yosisms <y.shemesh88@gmail.com>
2020-12-23 12:56:34 +0200

    Added registration validations with express-validator

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const { request } = require('express');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+const urlEncodedFalse = bodyParser.urlencoded({extended: false});
 app.use(express.json());
 
 app.use(session({
@@ -33,24 +35,12 @@ mongoose.connect(process.env.DB_PATH,  {useNewUrlParser: true, useUnifiedTopolog
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
-    username: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    firstName: {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
-    dateOfBirth: {
-        type: String
-    }
+    username: String,
+    password: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    dateOfBirth: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -76,39 +66,68 @@ app.get('/api/users', (req, res) => {
     }
 })
 
-app.post('/api/users/register', (req,res) => {
-    const requestBody = {
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        dateOfBirth: req.body.dateOfBirth
-    }
-    User.register(requestBody, req.body.password, (err, user) => {
-        if(err) {
-            console.log('error');
-            res.send({isValid: false, message: 'Username is taken'});
-        } else {
-            passport.authenticate('local')(req, res, () => {
-                const mailOption = {
-                    from: process.env.EMAIL,
-                    to: req.body.email,
-                    subject: 'Branded Members successfully registered',
-                    text: `Hello ${req.body.firstName}! \n Welcome to Branded Members. Your account was successfully created! `
-                }
-                
-                transporter.sendMail(mailOption, (err, data) => {
-                    if (err) {
-                        console.log('email error!', err)
-                    } else {
-                        console.log('email sent!')
-                    }
-                })
-                res.json({isValid: true})
-            })
+app.post('/api/users/register', urlEncodedFalse ,[
+    check('firstName', 'Must enter a first name.')
+        .exists()
+        .isLength({min: 1}),
+    check('lastName', 'Must enter a last name.')
+        .exists()
+        .isLength({min: 1}),
+    check('username', 'Username must be between 6 to 10 characters.')
+        .exists()
+        .isLength({ min: 6, max: 10 }),
+    check('email', 'Email is not valid.')
+        .isEmail()
+        .normalizeEmail(),
+    check('password', 'Password must be between 4 to 8 characters.')
+        .exists()
+        .isLength({ min: 4, max: 8}),
+    check('dateOfBirth', 'Date of birth not valid.')
+        .exists()
+        .isISO8601('yyyy-mm-dd')
+], (req,res) => {
+
+    const errors = validationResult(req);
+    if(errors.isEmpty()) {
+        const requestBody = {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            dateOfBirth: req.body.dateOfBirth
         }
-    })
-    
+        User.register(requestBody, req.body.password, (err, user) => {
+            if(err) {
+                console.log('error');
+                res.send([{
+                    isValid: false,
+                    msg: 'Username is taken.',
+                    param: 'username'
+                }]);
+            } else {
+                passport.authenticate('local')(req, res, () => {
+                    const mailOption = {
+                        from: process.env.EMAIL,
+                        to: req.body.email,
+                        subject: 'Branded Members successfully registered',
+                        text: `Hello ${req.body.firstName}! \n Welcome to Branded Members. Your account was successfully created! `
+                    }
+                    
+                    transporter.sendMail(mailOption, (err, data) => {
+                        if (err) {
+                            console.log('email error!', err)
+                        } else {
+                            console.log('email sent!')
+                        }
+                    })
+                    res.json(null)
+                })
+            }
+        })
+    } else {
+        console.log(errors.errors);
+        res.json(errors.errors);
+    }
 })
 
 app.post('/api/login', (req, res) => {
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useHistory} from 'react-router-dom';
 import axios from 'axios';
 import {useForm} from 'react-hook-form';
@@ -6,8 +6,23 @@ import {useForm} from 'react-hook-form';
 const RegisterForm = ({logUser}) => {
     const history = useHistory();
 
-    const {register, handleSubmit, errors} = useForm();
-    const [isUsernameTaken, setIsUsernameTaken] = useState(null);
+    const {register, handleSubmit} = useForm();
+    const [serverErrors, setServerErrors] = useState([]);
+    const [firstNameError, setFirstNameError] = useState(undefined);
+    const [lastNameError, setLastNameError] = useState(undefined);
+    const [usernameError, setUsernameError] = useState(undefined);
+    const [emailError, setEmailError] = useState(undefined);
+    const [passwordError, setPasswordError] = useState(undefined);
+    const [dateOfBirthError, setDateOfBirthError] = useState(undefined);
+
+    useEffect(() => {
+            setFirstNameError(serverErrors.find(err => err.param === 'firstName'));
+            setLastNameError(serverErrors.find(err => err.param === 'lastName'));
+            setUsernameError(serverErrors.find(err => err.param === 'username'));
+            setEmailError(serverErrors.find(err => err.param === 'email'));
+            setPasswordError(serverErrors.find(err => err.param === 'password'));
+            setDateOfBirthError(serverErrors.find(err => err.param === 'dateOfBirth'));
+    }, [serverErrors])
 
     const getDate = (dd, mm, yyyy) => {
         const date = new Date();
@@ -24,8 +39,12 @@ const RegisterForm = ({logUser}) => {
             },
             data: JSON.stringify(data)
         }).then((respond) => {
-            if(respond.data.isValid === false){
-                setIsUsernameTaken(respond.data.message);
+            if(respond.data){
+
+                    setServerErrors(respond.data);
+                if(respond.data[0].isValid === false){
+                    setServerErrors(respond.data);
+                }
             } else {
                 logUser();
                 history.push('/');
@@ -37,29 +56,34 @@ const RegisterForm = ({logUser}) => {
         <div className="formContainer">
             <form onSubmit={handleSubmit(onSubmit)} className="form">
                 <div className="form__group">
-                    <input type="text" ref={register({ required: true })} className={`form__input ${errors.firstName && 'form__input--error'}`} name="firstName" placeholder="First nane" id="firstName" autoComplete="off" />
-                    <label htmlFor="firstName" className="form__label">first name:</label>
+                    {firstNameError && <p className="form__error--register">{firstNameError.msg}</p>}
+                    <input type="text" ref={register} className={`form__input ${(firstNameError) && 'form__input--error'} `} name="firstName" placeholder="First nane" id="firstName" autoComplete="off" />
+                    <label htmlFor="firstName" className={`form__label ${(firstNameError) && 'form__label--error'} `}>first name:</label>
                 </div>
                 <div className="form__group">
-                    <input type="text" ref={register({ required: true })} className={`form__input ${errors.lastName && 'form__input--error'}`} name="lastName" placeholder="Last name" id="lastName" autoComplete="off" />
-                    <label htmlFor="lastName" className="form__label">last name:</label>
+                {lastNameError && <p className="form__error--register">{lastNameError.msg}</p>}
+                    <input type="text" ref={register} className={`form__input ${(lastNameError) && 'form__input--error'} `} name="lastName" placeholder="Last name" id="lastName" autoComplete="off" />
+                    <label htmlFor="lastName" className={`form__label ${(lastNameError) && 'form__label--error'} `}>last name:</label>
                 </div>
                 <div className="form__group">
-                    {isUsernameTaken && <p className="form__error--register">{isUsernameTaken}</p>}
-                    <input type="text" ref={register({ required: true })} className={`form__input ${(errors.username || isUsernameTaken) && 'form__input--error'} `} name="username" placeholder="Username" id="username" autoComplete="off" />
-                    <label htmlFor="username" className={`form__label ${(errors.username || isUsernameTaken) && 'form__label--error'} `}>username:</label>
+                    {usernameError && <p className="form__error--register">{usernameError.msg}</p>}
+                    <input type="text" ref={register} className={`form__input ${(usernameError) && 'form__input--error'} `} name="username" placeholder="Username" id="username" autoComplete="off" />
+                    <label htmlFor="username" className={`form__label ${(usernameError) && 'form__label--error'} `}>username:</label>
                 </div>
                 <div className="form__group">
-                    <input type="email" ref={register({ required: true })} className={`form__input ${errors.email && 'form__input--error'}`} name="email" placeholder="Email" id="email" autoComplete="off" />
-                    <label htmlFor="email" className="form__label">email:</label>
+                    {emailError && <p className="form__error--register">{emailError.msg}</p>}
+                    <input type="email" ref={register} className={`form__input ${(emailError) && 'form__input--error'} `} name="email" placeholder="Email" id="email" autoComplete="off" />
+                    <label htmlFor="email" className={`form__label ${(emailError) && 'form__label--error'} `}>email:</label>
                 </div>
                 <div className="form__group">
-                    <input type="Password" ref={register({ required: true })} className={`form__input ${errors.password && 'form__input--error'}`}  name="password" placeholder="password" id="password" autoComplete="off" />
-                    <label htmlFor="password" className="form__label">password:</label>
+                    {passwordError && <p className="form__error--register">{passwordError.msg}</p>}
+                    <input type="Password" ref={register} className={`form__input ${passwordError && 'form__input--error'}`}  name="password" placeholder="password" id="password" autoComplete="off" />
+                    <label htmlFor="password" className={`form__label ${(passwordError) && 'form__label--error'} `}>password:</label>
                 </div>
                 <div className="form__group">
-                    <input type="date" ref={register({ required: true })} max={getDate(0, 0, -18).toLocaleDateString('fr-CA')} min={getDate(0, 0, -120).toLocaleDateString('fr-CA')} className={`form__input ${errors.dateOfBirth && 'form__input--error'}`} name="dateOfBirth" id="dateOfBirth" autoComplete="off" />
-                    <label htmlFor="dateOfBirth" className="form__label">date of birth:</label>
+                    {dateOfBirthError && <p className="form__error--register">{dateOfBirthError.msg}</p>}
+                    <input type="date" ref={register} max={getDate(0, 0, -18).toLocaleDateString('fr-CA')} min={getDate(0, 0, -120).toLocaleDateString('fr-CA')} className={`form__input ${dateOfBirthError && 'form__input--error'}`} name="dateOfBirth" id="dateOfBirth" autoComplete="off" />
+                    <label htmlFor="dateOfBirth" className={`form__label ${(dateOfBirthError) && 'form__label--error'} `}>date of birth:</label>
                 </div>
                 <button type="submit" className="btn btn-lg btn-green">Register</button>
             </form>
